85f1ad72d58bf81f0bbb48d2cc84346ac96f4b52
mmccorm1ck <mmccorm1ck@outlook.com>
2024-11-01 22:19:48 +0000

    Set up simple HTTP server

--- index.tsx
+++ index.tsx
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3000,
+  fetch: fetchHandler
+});
+
+console.log("Running on port "+server.port);
+
+function fetchHandler(request:Request): Promise<Response> {
+  return new Response(Bun.file("index.html"));
+}
