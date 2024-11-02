873110d4cef30fc5fc6ccae1dcea6144da59798d
mmccorm1ck <mmccorm1ck@outlook.com>
2024-11-02 11:40:03 +0000

    Built basic website layout

--- index.tsx
+++ index.tsx
@@ -1,3 +1,9 @@
+import { renderToString } from "react-dom/server";
+import "dotenv/config";
+
+const platform = process.env.PLATFORM;
+const link     = process.env.LINK;
+
 const server = Bun.serve({
   port: 3000,
   fetch: fetchHandler
@@ -6,5 +12,25 @@ const server = Bun.serve({
 console.log("Running on port "+server.port);
 
 function fetchHandler(request:Request): Promise<Response> {
-  return new Response(Bun.file("index.html"));
+  const url = new URL(request.url);
+
+  if (url.pathname === "" || url.pathname === "/") {
+    return new Response(Bun.file("index.html"));
+  }
+  
+  if (url.pathname === "/smLink" && request.method === "GET") {
+    return new Response(renderToString(<SMLink/>));
+  }
+  
+  if (url.pathname === "/link" && request.method === "GET") {
+    return new Response()
+  }
+
+  return new Response("Not found", {status: 404});
+}
+
+function SMLink() {
+  return(
+    <a role = "button" href = {link} class = "outline secondary" >Check us out on {platform}</a>
+  ); 
 }
